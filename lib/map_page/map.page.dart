aa237176d0ff6a12a60751a0de05f072b5aa91f6
sanin <sanin@pietechsolution.com>
2024-08-12 09:35:35 +0530

    map page created

--- lib/map_page/map.page.dart
+++ lib/map_page/map.page.dart
@@ -1,1 +1,21 @@
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatelessWidget {
+  MapPage({super.key});
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+  static const CameraPosition _cameraPosition = CameraPosition(
+      target: LatLng(11.073819384430653, 75.94709776999998), zoom: 14);
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: _cameraPosition,
+      onMapCreated: (controller) => _controller.complete(controller),
+    );
+  }
+}
